e42788fb11445575c1b430a3b4ed4e285e24ae69
FartushkoY <yevgeshkaf@gmail.com>
2024-01-23 00:03:46 +0100

    Homework_22_01

--- src/_24_01_22/Task3.java
+++ src/_24_01_22/Task3.java
@@ -3,13 +3,14 @@ package _24_01_22;
 public class Task3 {
     public static void main(String[] args) {
         long x = 1;
+        int median = 14;
         System.out.println(1);
-        for (int i = 2; i <= 25; i++) {
+        for (int i = 2; i <= median; i++) {
             System.out.println(x + "1");
             x = x * 10;
-            if (i == 14) {
+            if (i == median) {
                 x = x / 10;
-                for (i = 13; i <= 24; i++) {
+                for (int j = median-1; j >= 2; j--) {
                     x = x / 10;
                     System.out.println(x + "1");
                 }
